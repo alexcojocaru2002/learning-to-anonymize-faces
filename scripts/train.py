ede5c5b0a8b9bc78711873610a46862841adaa0d
Lucian Tosa <luci.tosa11@gmail.com>
2025-06-08 15:17:55 +0200

    Zip batches

--- scripts/train.py
+++ scripts/train.py
@@ -14,46 +14,45 @@ def train(model, loader_video, loader_faces, T1, T2, lambda_weight, optimizer_d,
     video_iter = iter(loader_video)
     faces_iter = iter(loader_faces)
     for epoch in tqdm(range(T1)):
-        clips, labels = next(video_iter)
-        faces, flabels = next(faces_iter)
-        v = clips
-        f = faces
-        m_f = model.m(f)
-        # align m_f
-        detections = model.face_detection.detect_faces_yolo(m_f)
-        aligned_faces = []
-        for index, face in enumerate(m_f):
-            aligned_faces.append(align(face, detections[index]['bbox'], detections[index]['keypoints']))
-        
-        l_adv = adversarial_loss(m_f, f, flabels, model.d)
-        # argmax update on D
-        optimizer_d.zero_grad()
-        optimizer_m.zero_grad()
-        l_adv.backward()
-        optimizer_d.step()
-        optimizer_m.step()
+        for (clips, labels), (faces, flabels) in zip(video_iter, faces_iter):
+            v = clips
+            f = faces
+            m_f = model.m(f)
+            # align m_f
+            detections = model.face_detection.detect_faces_yolo(m_f)
+            aligned_faces = []
+            for index, face in enumerate(m_f):
+                aligned_faces.append(align(face, detections[index]['bbox'], detections[index]['keypoints']))
+            
+            l_adv = adversarial_loss(m_f, f, flabels, model.d)
+            # argmax update on D
+            optimizer_d.zero_grad()
+            optimizer_m.zero_grad()
+            l_adv.backward()
+            optimizer_d.step()
+            optimizer_m.step()
 
-        # input video frame v
-        bounding_boxes = model.face_detection.detect_faces_yolo(v)
-        l1 = 0
-        if len(bounding_boxes) > 0:
-            r_v, v_prime = model.cut_regions(v, bounding_boxes)
+            # input video frame v
+            bounding_boxes = model.face_detection.detect_faces_yolo(v)
+            l1 = 0
+            if len(bounding_boxes) > 0:
+                r_v, v_prime = model.cut_regions(v, bounding_boxes)
 
-            # Expects r_v to be (B, 3, H, W)
-            rv_prime = model.m(r_v)
-            v_prime = v_prime + rv_prime  # have to double check if adding images works like this
-            l1 = F.l1_loss(rv_prime, r_v)
-        else:
-            v_prime = v
+                # Expects r_v to be (B, 3, H, W)
+                rv_prime = model.m(r_v)
+                v_prime = v_prime + rv_prime  # have to double check if adding images works like this
+                l1 = F.l1_loss(rv_prime, r_v)
+            else:
+                v_prime = v
 
-        l_det_dict = model.a(v_prime, labels)
-        l_det = sum(l_det_dict.values()) # sum of the loss values
+            l_det_dict = model.a(v_prime, labels)
+            l_det = sum(l_det_dict.values()) # sum of the loss values
 
-        # argmin M, A update
-        final_loss = l_adv + l_det + lambda_weight * l1
-        optimizer_a.zero_grad()
-        final_loss.backward()
-        optimizer_a.step()
+            # argmin M, A update
+            final_loss = l_adv + l_det + lambda_weight * l1
+            optimizer_a.zero_grad()
+            final_loss.backward()
+            optimizer_a.step()
 
     #second for for fine tuning A
 
